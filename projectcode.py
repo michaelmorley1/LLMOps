213a2e38f812c69e5f770a084ba032326e08c2e2
Michael <m.morley3@universityofgalway.ie>
2024-05-08 11:29:33 +0000

    Update projectcode.py

--- projectcode.py
+++ projectcode.py
@@ -1,4 +1,10 @@
-#Main code file
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
